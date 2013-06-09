89396166ef6e2a5506055046dbc677b693f6e729
Jon Jarboe <jjarboe@coverity.com>
2013-06-08 22:09:14 -0500

    Add AdaControl script

---
+++ adacontrol_import.py
@@ -0,0 +1,46 @@
+import re
+import sys
+import os.path
+from coverity_import import CoverityIssueCollector, main, Issue
+
+class AdaControlCollector(CoverityIssueCollector):
+    '''
+    A simple collector for AdaControl reports.
+    '''
+    _report_re = re.compile(r'^(?P<file>.+):(?P<line>\d+):\d+:\s*.+:\s*(?P<subcategory>.+): (?P<description>.*)$', re.M)
+
+    def process(self, f):
+        '''
+        This method assumes that reports are isolated to a single line.
+        If your tool reports issues on multiple lines, or for some other
+        reason the report lines may not be reordered, you'll need to override
+        this method to handle things appropriately.
+        '''
+        for l in f:
+            if not l.strip(): continue
+            m = self._report_re.match(l)
+            if m:
+                f = m.groupdict()
+                msg = Issue(checker='AdaControl',
+                            tag = f['description'],
+                            description = f['description'],
+                            subcategory = f['subcategory'],
+                           )
+                # print '###', self._build_dir
+                if os.path.isabs(f['file']):
+                    msg.add_location(f['line'], f['file'])
+                else:
+                    msg.add_location(f['line'], os.path.join(self._build_dir,f['file']))
+                self._issues.add(msg)
+                self._files.add(f['file'])
+            elif l.strip() == 'Counts summary:':
+                break
+            else:
+                print 'Unrecognized input format:', l
+                sys.exit(-1)
+
+if __name__ == '__main__':
+    build_dir = '/'
+    if 'adacontrol_import.py' not in sys.argv[-2]:
+        build_dir = sys.argv[-2]
+    print AdaControlCollector(build_dir=build_dir).run(sys.argv[-1])
